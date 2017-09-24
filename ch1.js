460708ed6c0fba60bbeb4d5fa49cadaf2ad31e0f
Jordan Weir <Jordanjweir@gmail.com>
2017-09-24 16:05:36 -0400

    Improved record function for better testing, console output

--- ch1.js
+++ ch1.js
@@ -2,6 +2,13 @@ var _ = require('underscore');
 
 var results = [];
 
+// Utility function to better print to console.
+function record(action, fun) {
+  args = _.rest(arguments, 2);
+  console.log("\n",'RECORD: Begin', action,'with', args);
+  console.log('Result:', fun.apply(null, args),"\n");
+}
+
 function splat (fun) {
   return function(array) {
     return fun.apply(null, array);
@@ -14,10 +21,6 @@ function unsplat(fun) {
   };
 }
 
-function record(action, result) {
-  console.log('RECORD: Did', action,'and got:', result);
-}
-
 var addArrayElements = splat(function(x, y) {
   return x + y;
 });
@@ -25,7 +28,121 @@ var addArrayElements = splat(function(x, y) {
 var joinElements = unsplat(function(array) { return array.join(' ') });
 
 
+// Messy version of parseAge function
+function parseAge (age) {
+  if (!_.isString(age)) throw new Error("Expecting a string");
+  var a;
+
+  console.log("Attempting to parse an age");
+
+  a = parseInt(age, 10);
+
+  if (_.isNaN(a)) {
+    console.log(["Could not parse age::", age].join(' '));
+    a = 0;
+  }
+
+  return a;
+}
+
+// To clean things up, we abstract the notion of a errors, information, and warnings.
+function fail(thing) {
+  // throw new Error(thing);
+  console.log(['!!! CRITICAL ERROR:', thing].join(' '));
+}
+
+function warn(thing) {
+  console.log(["WARNING:", thing].join(' '));
+}
+
+function note(thing) {
+  console.log(["NOTE:", thing].join(' '));
+}
+
+// Clean parseAge leveraging new functions
+function newParseAge(age) {
+  if(!_.isString(age)) fail("Expecting a string");
+  var a;
+
+  note("Attempting to parse an age");
+  a = parseInt(age, 10);
+  
+  if (_.isNaN(a)) {
+    warn(["Could not parse age:", age].join(' '));
+    a = 0;
+  }
+
+  return a;
+}
+
+// Messy implementation of nth, a simple function to return the item at an index.
+function naiveNth(a, index) {
+  return a[index];
+}
+
+var letters = ["a", "b", "c"];
+
+// What we really want is to check if it's indexable though...
+function isIndexed(data) {
+  return _.isArray(data) || _.isString(data);
+}
+
+function nth(a, index) {
+  if (!_.isNumber(index)) fail("Expected a number as the index");
+  if (!isIndexed(a)) fail("Not supported on non-indexed type");
+  if ((index < 0) || (index > a.length -1)) fail ("Index value is out of bounds");
+
+  return a[index];
+}
+
+// Similarly, we can build a 'second' abstraction.
+function second(a) {
+  return nth(a, 1);
+}
+
+// Predicate usage.
+// Predicates in javascript are functions that always return true or false.
+// They can be very useful abstractions in a wide range of cases.
+// Consider Array#sort.  This 'seems' to work logically to sort numbers.
+// Consider a naiveSort function based on Array#sort.
+function naiveSort(arr) {
+  return arr.sort();
+}
+
+// naiveSort doesn't work in a range of cases because .sort() defaults to a string comparison.
+// It can also take a function to decide which value to return.
+// Predicates to the rescue
+function lessOrEqual(x, y) {
+  return x <= y;
+}
+
+function goodSort(arr, pred) {
+  return arr.sort(pred);
+}
 
 
-record('addArrayElements', addArrayElements([1,2,3]));
-record('joinElements', joinElements(1, 2));
+
+record('addArrayElements', addArrayElements, [1,2]);
+record('joinElements', joinElements, 1, 2);
+record('parseAge', parseAge, "12");
+record('parseAge', parseAge, "frob");
+record('newParseAge', newParseAge, "12");
+record('newParseAge', newParseAge, "frob");
+record('naiveNth', naiveNth, letters, 1);
+record('naiveNth', naiveNth, {}, 1);
+record('nth', nth, letters, 1);
+record('nth', nth, "abc", 0);
+record('nth', nth, {}, 1);
+record('nth', nth, letters, 4000);
+record('nth', nth, letters, 'aaaaa');
+record('second', second, ['a', 'b']);
+record('second', second, 'fogus');
+record('second', second, {});
+record('naiveSort', naiveSort, [2,3,-6,-108,42]);
+record('naiveSort', naiveSort, [0, -1, -2]);
+record('naiveSort', naiveSort, [2, 3, -1, -6, 0, -108, 42, 10]);
+note('Naive sort does a bad job because the default sort function is a string comparison!');
+record('goodSort', goodSort, [2,3,-6,-108,42], lessOrEqual);
+record('goodSort', goodSort, [0, -1, -2], lessOrEqual);
+record('goodSort', goodSort, [2, 3, -1, -6, 0, -108, 42, 10], lessOrEqual);
+console.log([2, 3, -1, -6, 0, -108, 42, 10].sort(lessOrEqual));
